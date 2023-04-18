dd9340dceeea1f5a84cfd846ea10948ce44382cb
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-04-18 15:58:55 +0900

    0418 병합

--- src/main/java/com/spring/core/person/Person.java
+++ src/main/java/com/spring/core/person/Person.java
@@ -2,14 +2,16 @@ package com.spring.core.person;
 
 import lombok.*;
 
-@Setter @Getter
-@ToString @EqualsAndHashCode
-@NoArgsConstructor  // 기본생성자
+@Setter @Getter @ToString @EqualsAndHashCode
+@NoArgsConstructor // 기본 생성자
 @AllArgsConstructor // 모든 파라미터 생성자
-//Data - 한방에 해줌. 실무에선 사용 X
+
+//@Data // 실무에서는 사용 안하는게 좋다. 예를들어 세터를 빼야할때는 못뺀다.
+      // 위에있는 전체를 만들어준다.
 public class Person {
 
-    private String niceName;
-    private int age;
+    private String nickName;
     private String address;
+    private int age;
+
 }
